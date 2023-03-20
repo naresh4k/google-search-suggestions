e4d093a32480a50c475b197b6e30d5ecf4054184
naresh4k <nareshk.exclusive@gmail.com>
2023-03-20 11:16:46 +0000

    Google search suggestions using react

---
+++ src/components/SuggestionItem/index.js
@@ -0,0 +1,24 @@
+import './index.css'
+
+const SuggestionItem = props => {
+  const {suggestionDetails, updateSearchInput} = props
+  const {suggestion} = suggestionDetails
+
+  const onClickSuggestion = () => {
+    updateSearchInput(suggestion)
+  }
+
+  return (
+    <li>
+      <p>{suggestion}</p>
+      <button type="button" onClick={onClickSuggestion}>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png"
+          alt="arrow"
+        />
+      </button>
+    </li>
+  )
+}
+
+export default SuggestionItem
